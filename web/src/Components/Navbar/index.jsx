3920b975047da2ecf69cd48c76b53cd79caba1f0
stojanov97 <stojanov091@gmail.com>
2024-04-11 23:57:38 +0200

    pc to laptop

---
+++ web/src/Components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+const Navbar = () => {
+  return (
+    <nav id='navbar'>
+        <div>Home</div>
+        <div>About</div>
+        <div>Contact</div>
+    </nav>
+  )
+}
+
+export default Navbar
